be5006c03beedb6ccf627de5c0b4d50cb4f82d8f
parilbeladiya <parilbeladiya>
2023-04-09 23:21:12 +0530

    changes in modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal actionLabel="Submit" secondryActionLabel="Cancel" title="Hello World" isOpen />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
