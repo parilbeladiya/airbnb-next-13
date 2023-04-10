d1c11949ada935b8041cb9a7af35e82040713e45
parilbeladiya <parilbeladiya>
2023-04-10 23:45:26 +0530

    setup prisma

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,57 +1,50 @@
-'use client'
+"use client";
 
-import { AiOutlineMenu } from "react-icons/ai"
-import Avatar from "../Avatar"
-import { useCallback, useState } from "react"
-import MenuItem from "./MenuItem"
-import useRegisterModal from "@/app/hooks/useRegisterModal"
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
-    const registerModal = useRegisterModal();
-    const [isOpen, setIsOpen] = useState(false)
+  const registerModal = useRegisterModal();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleOpen = useCallback(() => {
-        setIsOpen((value) => !value)
-    }, [])
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
 
   return (
     <div className="relative">
-        <div className="flex flex-row items-center gap-3">
-            <div 
-                className="hidden md:block text-sm font-semibold py-2 px-4 rounded-full transition hover:bg-neutral-100 cursor-pointer" 
-                onClick={() => {}}
-            >
-                Airbnb your home
-            </div>
-            <div 
-                className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full hover:shadow-md transition cursor-pointer"
-                onClick={toggleOpen}
-            >
-                <AiOutlineMenu />
-                <div className="hidden md:block">
-                    <Avatar />
-                </div>
-            </div>
+      <div className="flex flex-row items-center gap-3">
+        <div
+          className="hidden md:block text-sm font-semibold py-2 px-4 rounded-full transition hover:bg-neutral-100 cursor-pointer"
+          onClick={() => {}}
+        >
+          Airbnb your home
         </div>
+        <div
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full hover:shadow-md transition cursor-pointer"
+          onClick={toggleOpen}
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
 
-        {isOpen && (
-            <div className="absolute right-0 top-12 overflow-hidden rounded-xl shadow-md text-sm w-[40vw] md:w-3/4 bg-white">
-                <div className="flex flex-col items-center">
-                    <>
-                        <MenuItem
-                            onClick={() => {}}
-                            label="Login"
-                        />
-                         <MenuItem
-                            onClick={registerModal.onOpen}
-                            label="Sign up"
-                        />
-                    </>
-                </div>
-            </div>
-        )}
-      
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col items-center">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={registerModal.onOpen} label="Sign up" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
-export default UserMenu
+  );
+};
+export default UserMenu;
